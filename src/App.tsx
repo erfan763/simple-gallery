17ea61f88e2cafa0a0fcadee40a99f7eb9f9fc80
erfan763 <erfanalikhani6@gmail.com>
2022-12-26 14:35:23 +0330

    some good changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
-import { Form, Formik } from "formik";
+import Images from "./components/images";
+import { Box, Divider } from "@mui/material";
+
+import "react-toastify/dist/ReactToastify.min.css";
+import { ToastContainer } from "react-toastify";
+import CustomeDrawer from "./components/CustomeDrawer";
+import { useState } from "react";
 
 import image1 from "./assets/1.jpeg";
 import image2 from "./assets/2.jpeg";
@@ -19,72 +25,51 @@ import image16 from "./assets/16.jpeg";
 import image17 from "./assets/17.jpeg";
 import image18 from "./assets/18.jpeg";
 
-import Images from "./components/images";
-import { Box, Divider } from "@mui/material";
-
-import "react-toastify/dist/ReactToastify.min.css";
-import { ToastContainer } from "react-toastify";
-import CustomeDrawer from "./components/CustomeDrawer";
-import { useState } from "react";
-
 function App() {
   const [isDark, setIsDark] = useState<boolean>(false);
+
+  const [myImage, setMyImage] = useState<any[]>([
+    { src: image1 },
+    { src: image2 },
+    { src: image3 },
+    { src: image4 },
+    { src: image5 },
+    { src: image6 },
+    { src: image7 },
+    { src: image8 },
+    { src: image9 },
+    { src: image10 },
+    { src: image11 },
+    { src: image12 },
+    { src: image13 },
+    { src: image14 },
+    { src: image15 },
+    { src: image16 },
+    { src: image17 },
+    { src: image18 },
+  ]);
 
   return (
     <>
       <ToastContainer style={{ width: "auto" }} position="top-right" />
       <Box>
-        <Formik
-          initialValues={{
-            gallery: [
-              { src: image1 },
-              { src: image2 },
-              { src: image3 },
-              { src: image4 },
-              { src: image5 },
-              { src: image6 },
-              { src: image7 },
-              { src: image8 },
-              { src: image9 },
-              { src: image10 },
-              { src: image11 },
-              { src: image12 },
-              { src: image13 },
-              { src: image14 },
-              { src: image15 },
-              { src: image16 },
-              { src: image17 },
-              { src: image18 },
-            ],
+        <Box
+          sx={{
+            width: "100%",
+            bgcolor: isDark ? "rgb(10, 25, 41)" : "unset",
+            m: 0,
+            pb: 50,
+            minHeight: "100vh",
           }}
-          onSubmit={(values) => {
-            console.log(values);
-          }}
-          enableReinitialize={true}
-          render={({ values, setFieldValue }) => (
-            <Form
-              style={{
-                width: "100%",
-                background: isDark ? "rgb(10, 25, 41)" : "unset",
-                margin: 0,
-                paddingBottom: 50,
-                minHeight: "100vh",
-              }}
-            >
-              {/* <Switch defaultChecked /> */}
-              <Box dir="rtl" display="flex" height="64px" px={6} pt={2}>
-                <CustomeDrawer isDark={isDark} setIsDark={setIsDark} />
-              </Box>
-              <Divider />
+        >
+          {/* <Switch defaultChecked /> */}
+          <Box dir="rtl" display="flex" height="64px" px={6} pt={2}>
+            <CustomeDrawer isDark={isDark} setIsDark={setIsDark} />
+          </Box>
+          <Divider />
 
-              <Images
-                isDark={isDark}
-                values={values}
-                setFieldValue={setFieldValue}
-              />
-            </Form>
-          )}
-        />
+          <Images myImage={myImage} setMyImage={setMyImage} isDark={isDark} />
+        </Box>
       </Box>
     </>
   );
--- src/components/images.tsx
+++ src/components/images.tsx
@@ -8,14 +8,11 @@ import {
   Select,
   styled,
 } from "@mui/material";
-import { FieldArray } from "formik";
-
 import AddIcon from "@mui/icons-material/Add";
 import DeleteIcon from "@mui/icons-material/Delete";
 import ZoomInIcon from "@mui/icons-material/ZoomIn";
 
 import { useState } from "react";
-import { TonalitySharp } from "@mui/icons-material";
 import { toast } from "react-toastify";
 
 const Input = styled("input")({
@@ -23,47 +20,60 @@ const Input = styled("input")({
 });
 
 const coloumns = [
-  { value: "1fr 1fr 1fr 1fr 1fr", number: 5, size: "180px" },
-  { value: "1fr 1fr 1fr 1fr 1fr 1fr", number: 6, size: "150px" },
-  { value: "1fr 1fr 1fr 1fr 1fr 1fr 1fr", number: 7, size: "140px" },
-  { value: "1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr", number: 8, size: "130px" },
-  { value: "1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr", number: 9, size: "120px" },
+  { value: "1fr 1fr 1fr 1fr 1fr", number: 5, width: "15vw", height: "20vh" },
+  {
+    value: "1fr 1fr 1fr 1fr 1fr 1fr",
+    number: 6,
+    width: "12vw",
+    height: "20vh",
+  },
+  {
+    value: "1fr 1fr 1fr 1fr 1fr 1fr 1fr",
+    number: 7,
+    width: "10vw",
+    height: "18vh",
+  },
+  {
+    value: "1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr",
+    number: 8,
+    width: "8vw",
+    height: "16vh",
+  },
+  {
+    value: "1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr",
+    number: 9,
+    width: "6vw",
+    height: "10vh",
+  },
   {
     value: "1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr",
     number: 10,
-    size: "110px",
-  },
-  {
-    value: "1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr",
-    number: 11,
-    size: "80px",
+    width: "6vw",
+    height: "10vh",
   },
 ];
 
 export default function Images({
-  values,
-  setFieldValue,
   isDark,
+  setMyImage,
+  myImage,
 }: {
-  values: {
-    gallery: {
-      src: string;
-    }[];
-  };
-  setFieldValue: (
-    field: string,
-    value: any,
-    shouldValidate?: boolean | undefined
-  ) => void;
   isDark: boolean;
+  setMyImage: React.Dispatch<React.SetStateAction<any[]>>;
+  myImage: any[];
 }) {
   const [fileUrl, setFileUrl] = useState<any>();
   const [open, setOpen] = useState<boolean>(false);
   const [imageZoom, setImageZoom] = useState<any>();
 
-  const [coloumnsFr, setColumnsFr] = useState<{ fr: string; size: string }>({
+  const [coloumnsFr, setColumnsFr] = useState<{
+    fr: string;
+    width: string;
+    height: string;
+  }>({
     fr: "1fr 1fr 1fr 1fr 1fr",
-    size: "180px",
+    width: "15vw",
+    height: "20vh",
   });
 
   const handleClose = () => {
@@ -107,7 +117,6 @@ export default function Images({
           <InputLabel
             id="demo-simple-select-label"
             sx={{ color: isDark ? "rgba(255, 255, 255, 0.7)" : "" }}
-            // sx={{ color: isDark ? "primary" : "unset" }}
           >
             Image per row
           </InputLabel>
@@ -119,7 +128,8 @@ export default function Images({
               const d = coloumns?.find((i) => i?.value === e.target.value);
               setColumnsFr({
                 fr: String(e.target.value),
-                size: String(d?.size),
+                width: String(d?.width),
+                height: String(d?.height),
               });
             }}
             label="image per row"
@@ -135,109 +145,97 @@ export default function Images({
           </Select>
         </FormControl>
       </Box>
-      <FieldArray
-        name="gallery"
-        render={(arrayHelpers) => (
-          <Box display="grid" gridTemplateColumns={coloumnsFr?.fr} gap={2}>
-            {values?.gallery.map((v: any, index) => {
-              if (fileUrl) {
-                setFieldValue(
-                  `gallery[${values?.gallery?.length - 1}].src`,
-                  fileUrl?.avatar
-                );
-                setFileUrl(undefined);
-              }
-              return (
-                <Box
-                  display="flex"
-                  justifyContent="center"
-                  alignItems="center"
-                  p={2}
-                  height="100%"
-                  position="relative"
-                >
-                  <Avatar
-                    src={v?.src}
-                    alt=""
-                    sx={{
-                      width: coloumnsFr?.size,
-                      height: coloumnsFr?.size,
-                      border: isDark
-                        ? "solid 4px #FFFFFF"
-                        : "solid 2px #000000",
-                      boxShadow: "0 1px 3px rgba(34, 25, 25, 0.4)",
-                    }}
-                    variant="rounded"
-                  />
-                  <Box
-                    sx={{
-                      "&:hover": {
-                        zIndex: 10,
-                        opacity: 1,
-                        cursor: "pointer",
-                      },
-                      opacity: 0,
-                    }}
-                    top="0"
-                    display="flex"
-                    justifyContent="center"
-                    alignItems="center"
-                    width="100%"
-                    height="100%"
-                    position="absolute"
-                  >
-                    <Box
-                      onClick={() => {
-                        arrayHelpers.remove(index);
-                        toast.success("Image deleted successfully");
-                      }}
-                    >
-                      <DeleteIcon sx={{ width: "45px", height: "45px" }} />
-                    </Box>
-                    <Box onClick={() => handleOpen(v.src)}>
-                      <ZoomInIcon sx={{ width: "45px", height: "45px" }} />
-                    </Box>
-                  </Box>
-                </Box>
-              );
-            })}
-            <label htmlFor="h">
-              <Input
-                accept="image/*"
-                id="h"
-                name="files"
-                multiple
-                type="file"
-                color="info"
-                onChange={(e: any) => {
-                  const avatar = e.target.files && e.target.files[0];
-                  const imageUrl = URL.createObjectURL(avatar);
-                  setFileUrl({ avatar: imageUrl });
-                  arrayHelpers.push({ src: "" });
-                  toast.success("Image added successfully");
+
+      <Box display="grid" gridTemplateColumns={coloumnsFr?.fr} gap={1}>
+        {myImage?.map((v: any, index: any) => {
+          if (fileUrl) {
+            setMyImage([...myImage, { src: fileUrl.avatar }]);
+            setFileUrl(undefined);
+          }
+          return (
+            <Box
+              key={index}
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              p={2}
+              height="100%"
+              position="relative"
+            >
+              <Avatar
+                src={v?.src}
+                alt=""
+                sx={{
+                  width: coloumnsFr?.width,
+                  height: coloumnsFr?.height,
+                  border: isDark ? "solid 4px #FFFFFF" : "solid 2px #000000",
+                  boxShadow: "0 1px 3px rgba(34, 25, 25, 0.4)",
                 }}
+                variant="rounded"
               />
               <Box
                 sx={{
-                  width: "100%",
-                  height: "100%",
-                  cursor: "pointer",
-                  display: "flex",
-                  justifyContent: "center",
+                  "&:hover": {
+                    zIndex: 10,
+                    opacity: 1,
+                    cursor: "pointer",
+                  },
+                  opacity: 0,
                 }}
+                top="0"
                 display="flex"
                 justifyContent="center"
                 alignItems="center"
+                width="100%"
+                height="100%"
+                position="absolute"
               >
-                <AddIcon
-                  color="primary"
-                  sx={{ width: "80px", height: "80px" }}
-                />
+                <Box
+                  onClick={() => {
+                    setMyImage(myImage.filter((a) => a.src !== v.src));
+                    toast.success("Image deleted successfully");
+                  }}
+                >
+                  <DeleteIcon sx={{ width: "45px", height: "45px" }} />
+                </Box>
+                <Box onClick={() => handleOpen(v.src)}>
+                  <ZoomInIcon sx={{ width: "45px", height: "45px" }} />
+                </Box>
               </Box>
-            </label>
+            </Box>
+          );
+        })}
+        <label htmlFor="h">
+          <Input
+            accept="image/*"
+            id="h"
+            name="files"
+            multiple
+            type="file"
+            color="info"
+            onChange={(e: any) => {
+              const avatar = e.target.files && e.target.files[0];
+              const imageUrl = URL.createObjectURL(avatar);
+              setFileUrl({ avatar: imageUrl });
+              toast.success("Image added successfully");
+            }}
+          />
+          <Box
+            sx={{
+              width: "100%",
+              height: "100%",
+              cursor: "pointer",
+              display: "flex",
+              justifyContent: "center",
+            }}
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <AddIcon color="primary" sx={{ width: "80px", height: "80px" }} />
           </Box>
-        )}
-      />
+        </label>
+      </Box>
     </>
   );
 }
